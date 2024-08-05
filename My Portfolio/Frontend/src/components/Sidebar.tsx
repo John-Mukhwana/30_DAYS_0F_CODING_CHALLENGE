001d1cb76111e68475988bd8b01efefb0ef0d6e2
AJD254 <jontexjesus@gmail.com>
2024-08-05 22:14:47 +0300

    implemented theresponsiveness

--- My Portfolio/Frontend/src/components/Sidebar.tsx
+++ My Portfolio/Frontend/src/components/Sidebar.tsx
@@ -1,6 +1,8 @@
+
+
 import React, { useState, useEffect } from 'react';
-import { FaFileAlt,FaUserCircle,FaHome,FaProjectDiagram, FaGithub } from 'react-icons/fa';
-import { MdSchool, MdMenu, MdStar, MdPhone} from 'react-icons/md';
+import { FaFileAlt, FaUserCircle, FaHome, FaProjectDiagram, FaGithub } from 'react-icons/fa';
+import { MdSchool, MdMenu, MdStar, MdPhone } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 interface SidebarProps {
@@ -37,8 +39,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, toggleSidebar }) => {
     };
 
     return (
-        <aside className={`fixed z-40 max-h-screen bg-emerald-600 dark:bg-gray-900 shadow-2xl transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0 md:w-64`}>
-            <div className="flex flex-col h-full overflow-y-auto">
+        <aside className={`fixed z-40 bg-emerald-600 dark:bg-gray-900 shadow-2xl transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0 md:w-64 md:flex md:flex-col`}>
+            <div className="flex  flex-col h-full overflow-y-auto">
                 <div className='flex justify-between items-center bg-black dark:bg-white w-full h-10 px-4'>
                     <h2 className="text-2xl font-semibold text-white dark:text-black">Admin Panel</h2>
                     <button className="md:hidden text-white dark:text-black" onClick={toggleSidebar}>
@@ -51,48 +53,48 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, toggleSidebar }) => {
                         alt="Profile"
                         className="w-24 h-24 rounded-full border-2 border-gray-300"
                     />
-                    <p className="mt-2 text-2xl font-bold text-yellow-400 dark:text-white">{userName}</p>
+                    <p className="mt-2   text-2xl font-bold text-yellow-400 dark:text-white">{userName}</p>
                 </div>
                 <div className="flex-grow overflow-y-auto">
-                    <ul className="space-y-3 pt-10 font-bold w-full">
+                    <ul className="space-y-2 pt-10 font-bold w-full">
                         <li>
                             <Link to="AdminHome" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                <FaHome className='text-white text-4xl font-bold' /><span className="ml-3 text-lg">Home</span>
+                                <FaHome className="text-white text-2xl" /><span className="ml-3 text-lg">Home</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="Users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                <FaUserCircle className='text-white text-4xl font-bold' /><span className="ml-3">About</span>
+                                <FaUserCircle className="text-white text-2xl " /><span className="ml-3 text-lg">About</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="bookings" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                <FaProjectDiagram className='text-white text-4xl font-bold' /><span className="ml-3">Projects</span>
+                                <FaProjectDiagram className="text-white text-2xl" /><span className="ml-3 text-lg">Projects</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="AdminVehicleTable" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                < MdStar className='text-white text-4xl font-bold' /><span className="ml-3">Skills</span>
+                                <MdStar className="text-white text-2xl" /><span className="ml-3 text-lg">Skills</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="LocationAndBranches" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                < MdSchool className='text-white text-4xl font-bold' /><span className="ml-3 text-lg">Education</span>
+                                <MdSchool className="text-white text-2xl" /><span className="ml-3 text-lg">Education</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="ticketsPage" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                <FaFileAlt  className='text-white text-4xl font-bold' /><span className="ml-3 text-lg">Resume</span>
+                                <FaFileAlt className="text-white text-2xl" /><span className="ml-3 text-lg">Resume</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="FleetsPage" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                < FaGithub className='text-white text-4xl font-bold' /><span className="ml-3 text-lg">Top Repositories</span>
+                                <FaGithub className="text-white text-2xl" /><span className="ml-3 text-lg">Top Repositories</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="Reports" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-900 hover:text-white dark:hover:text-yellow-500" onClick={handleLinkClick}>
-                                <MdPhone className='text-white text-4xl font-bold' /><span className="ml-3 text-lg">Contact Me</span>
+                                <MdPhone className="text-white text-2xl" /><span className="ml-3 text-lg ">Contact Me</span>
                             </Link>
                         </li>
                     </ul>
--- My Portfolio/Frontend/src/components/footer.tsx
+++ My Portfolio/Frontend/src/components/footer.tsx
@@ -1,12 +1,15 @@
+
 const Footer = () => {
-    return (
-      <footer className="bg-blue-800 dark:bg-gray-700 text-white py-4 mt-auto">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2024 <span className="text-yellow-400 font-bold">EXO</span><span className="text-white">Travel</span>. All rights reserved.</p>
-        </div>
-      </footer>  
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-sky-800 dark:bg-gray-700 text-white py-4 mt-auto">
+      <div className="container mx-auto px-4 text-center sm:flex sm:justify-between sm:items-center">
+        <p className="text-sm sm:text-base">
+          &copy; 2024 <span className="text-yellow-400 font-bold">John</span>
+          <span className="text-white">Bradill</span>. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
